65ee70b2b12bf136d27b2e24f3f672217725ccbc
Keerthana <darapukeerthana2808@gmail.com>
2025-10-16 05:07:19 +0530

    modified

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ function App() {
       
         <h1>CI/CD and Cloud DevOps Course</h1>
         <p>This is a simple homepage deployed on GitHub Actions using Vite.</p>
-        <h1>Done with Git Pages & Git Actions</h1>
+        <h1>Done with Git Actions</h1>
     </div>
   )
 }
