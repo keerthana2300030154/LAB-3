9277160637e2177e27949f7efa4d596b916c7d78
Keerthana <darapukeerthana2808@gmail.com>
2025-10-16 04:55:54 +0530

    Updated App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+import './style.css'
+
+
+function App() {
+
+  return (
+    <div>
+      
+        <h1>CI/CD and Cloud DevOps Course</h1>
+        <p>This is a simple homepage deployed on GitHub Actions using Vite.</p>
+        <h1>Done with Git Pages & Git Actions</h1>
+    </div>
+  )
+}
+
+export default App
